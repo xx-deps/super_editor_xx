128def423d72a92fff9eab1d4c14a32a2a2284fd
xieyi1997 <2649911478@qq.com>
2025-08-01 13:31:10 +0800

    rm

--- packages/linkify/example/linkify_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:linkify/linkify.dart';
-
-void main() {
-  print(linkify("Made by https://cretezy.com person@example.com"));
-  // Output: [TextElement: 'Made by ', UrlElement: 'https://cretezy.com' (cretezy.com), TextElement: ' ', EmailElement: 'person@example.com' (person@example.com)]
-}
